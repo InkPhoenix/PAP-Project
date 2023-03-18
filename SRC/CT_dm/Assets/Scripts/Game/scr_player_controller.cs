a6945082fe0305b1345d3e24864df822b6d47b25
Ink Phoenix <99095585+InkPhoenix@users.noreply.github.com>
2023-03-18 15:45:26 +0000

    18-3-2023_1545

--- SRC/CT_dm/Assets/Scripts/Game/scr_player_controller.cs
+++ SRC/CT_dm/Assets/Scripts/Game/scr_player_controller.cs
@@ -3,39 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-//[RequireComponent(typeof(CharacterController))]
 public class scr_player_controller : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed = 2.0f;
-    [SerializeField] private float jumpHeight = 1.0f;
-    [SerializeField] private float rotationSpeed = 4f;
-    //public GameObject obj_player;
-
-    //private PlayerInput playerInput;
     private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool grounded_player;
-    //private Transform cameraMainTransform;
 
     private Vector3 movement;
+    private Vector3 modified_movement;
     private bool is_movement_pressed;
-    private bool is_jumping;
-    public float rotation_factor_per_frame = 10f;
+    private float player_speed = 4.0f;
+    private float rotation_factor_per_frame = 10f; //how fast the player rotates
 
-    private void Start()
+    private float grounded_gravity = -0.05f;          //gravity when on the ground
+    private float player_gravity = Physics.gravity.y; //gravity when off the ground
+    private float terminal_velocity = -15.0f;
+
+    private float max_jump_height = 2.0f;
+    private float max_jump_time = 0.75f;
+    private bool is_jumping = false;
+    private float initial_jump_velocity;
+    private int max_jumps = 2;
+    private int jumps = 0;
+    private float double_jump_multiplier = 0.8f; //force multiplier for jumps after the first one
+
+    private void Awake()
     {
         controller = GetComponent<CharacterController>();
-        //playerInput = GetComponent<PlayerInput>();
-        //cameraMainTransform = Camera.main.transform;
+
+        //setup jump variables
+        float time_to_apex = max_jump_time / 2;
+        player_gravity = (-2 * max_jump_height) / Mathf.Pow(time_to_apex, 2);
+        initial_jump_velocity = (2 * max_jump_height) / time_to_apex;
     }
 
     public void moving(InputAction.CallbackContext context)
     {
         if (context.performed) //key pressed
         {
-            movement.x = context.ReadValue<Vector2>().x;
-            //movement.y = Physics.gravity.y;
-            movement.z = context.ReadValue<Vector2>().y;
+            movement.x = context.ReadValue<Vector2>().x * player_speed;
+            movement.z = context.ReadValue<Vector2>().y * player_speed;
 
             if (movement.x != 0 || movement.z != 0) { is_movement_pressed = true; }
             else { is_movement_pressed = false; }
@@ -44,7 +49,6 @@ public class scr_player_controller : MonoBehaviour
         if (context.canceled) //key released
         {
             movement.x = context.ReadValue<Vector2>().x;
-            //movement.y = Physics.gravity.y;
             movement.z = context.ReadValue<Vector2>().y;
 
             if (movement.x != 0 || movement.z != 0) { is_movement_pressed = true; }
@@ -71,81 +75,62 @@ public class scr_player_controller : MonoBehaviour
 
     private void playerGravity()
     {
+        bool is_falling = movement.y <= 0.0f || !is_jumping;
+        float fall_multiplier = 2.0f;
+
         if (controller.isGrounded)
         {
-            float grounded_gravity = -0.05f;
             movement.y = grounded_gravity;
+            modified_movement.y = grounded_gravity;
         }
-        else { movement.y = Physics.gravity.y; }
+        else if(is_falling)
+        {
+            float previous_Y_velocity = movement.y;
+            movement.y = movement.y + (player_gravity * fall_multiplier * Time.deltaTime);
+            modified_movement.y = Mathf.Max((previous_Y_velocity + movement.y) * 0.5f, terminal_velocity);
+        }
+        else
+        {
+            float previous_Y_velocity = movement.y;
+            movement.y = movement.y + (player_gravity * Time.deltaTime);
+            modified_movement.y = (previous_Y_velocity + movement.y) * 0.5f;
+        }
     }
 
     public void jump(InputAction.CallbackContext context)
     {
-        if (context.performed) { is_jumping = true; }
-        if (context.canceled) { is_jumping = false; }
+        if (context.started) //key pressed
+        {
+            if (jumps != max_jumps)
+            {
+                jumps++;
+                is_jumping = context.ReadValueAsButton();
+                if (jumps > 1)
+                {
+                    movement.y = (initial_jump_velocity) * double_jump_multiplier;
+                    modified_movement.y = (initial_jump_velocity) * double_jump_multiplier;
+                }
+                else
+                {
+                    movement.y = initial_jump_velocity;
+                    modified_movement.y = initial_jump_velocity;
+                }
+            }
+        }
+
+        if (context.canceled) { is_jumping = context.ReadValueAsButton(); } //key released
     }
 
     void Update()
     {
-        Debug.Log($"is moving?: {is_movement_pressed} || {movement} || playerVelocity.y = {playerVelocity.y} || is jumping? = {is_jumping} || is grounded?: {grounded_player}");
+        Debug.Log($"is moving?: {is_movement_pressed} || movement: {movement} || grounded gravity {grounded_gravity} || is jumping? = {is_jumping} || jumps: {jumps} || controller.isGrounded: {controller.isGrounded}");
         playerRotation();
+
+        modified_movement.x = movement.x;
+        modified_movement.z = movement.z;
+        controller.Move(modified_movement * Time.deltaTime);
+
         playerGravity();
-
-        //grounded_player = controller.isGrounded;
-        //if (grounded_player && playerVelocity.y < 0) { playerVelocity.y = 0f; }
-
-        //Jump
-        //if (is_jumping == true)
-        //{
-        //    playerVelocity.y += Mathf.Sqrt(jumpHeight * -1.0f * Physics.gravity.y);
-        //}
-        //playerVelocity.y += Physics.gravity.y * Time.deltaTime;
-
-        //controller.Move(playerVelocity * Time.deltaTime);
-
-        controller.Move(movement * playerSpeed * Time.deltaTime);
-
-        //angle player faces depending on the camera
-        //if (movement != Vector3.zero)
-        //{
-        //    float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
-        //    Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
-        //    transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
-        //}
-
-
-
-
-
-
-        /*groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
-        Vector2 movement = playerInput.actions["Movement"].ReadValue<Vector2>();
-        Vector3 move = new Vector3(movement.x, 0, movement.y);
-        move = cameraMainTransform.forward * move.z + cameraMainTransform.right * move.x;
-        move.y = 0f;
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        //Jump
-        if (playerInput.actions["Jump"].triggered && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * Physics.gravity.y);
-        }
-
-        /*playerVelocity.y += Physics.gravity.y * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
-
-        if (movement != Vector2.zero)
-        {
-            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMainTransform.eulerAngles.y;
-            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
-        }*/
-        //Debug.Log($"playerVelocity & move: {playerVelocity} | {move}");
-        //Debug.Log($"obj_player.transform.localPosition: {obj_player.transform.localPosition}");
+        if (controller.isGrounded) { jumps = 0; }
     }
 }
