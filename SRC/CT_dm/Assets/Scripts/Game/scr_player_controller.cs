8ee4c45165a0dd2f435f8e9608cff4d43341838b
Ink Phoenix <99095585+InkPhoenix@users.noreply.github.com>
2023-06-28 04:45:02 +0100

    28-6-2023_0445

--- SRC/CT_dm/Assets/Scripts/Game/scr_player_controller.cs
+++ SRC/CT_dm/Assets/Scripts/Game/scr_player_controller.cs
@@ -5,6 +5,164 @@ using UnityEngine.InputSystem;
 
 public class scr_player_controller : MonoBehaviour
 {
+    public GameObject player_cam;
+
+    public Transform orientation;
+    public Transform player;
+    public Transform obj_player;
+    public Rigidbody rigidbd;
+
+    private Vector2 input_movement;
+
+    //movement vars
+    private float rotation_speed = 7f;
+    private float move_speed = 4f;
+    private Vector3 move_direction;
+
+    //ground vars
+    private float player_height = 1.8f;
+    public LayerMask what_is_ground;
+    private bool grounded;
+    private float ground_drag = 5f;
+
+    //jump vars
+    private float jump_force = 7f;
+    private float jump_cooldown = 0.25f;
+    private float air_multiplier = 0.4f;
+    private bool ready_to_jump;
+    private bool is_jumping;
+
+    private void Start()
+    {
+        rigidbd = GetComponent<Rigidbody>();
+        rigidbd.freezeRotation = true;
+        ready_to_jump = true;
+    }
+
+    public void Update()
+    {
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, player_height * 0.5f + 0.2f, what_is_ground);
+
+        //handle drag
+        if (grounded) { rigidbd.drag = ground_drag; }
+        else { rigidbd.drag = 0; }
+
+        speedControl();
+
+        if (is_jumping) //continue jumping if jump key is held
+        {
+            if (ready_to_jump && grounded)
+            {
+                ready_to_jump = false;
+                Jump();
+                Invoke(nameof(resetJump), jump_cooldown);
+            }
+        }
+
+        //rotate orientation
+        Vector3 view_direction = player.position - new Vector3(player_cam.transform.position.x, player.position.y, player_cam.transform.position.z);
+        //Debug.Log($"player view_direction: {view_direction}");
+        if (view_direction != Vector3.zero) { orientation.forward = view_direction.normalized; } //if check needed to get rid of error message
+        //Debug.Log($"player orientation.forward: {orientation.forward}");
+        //Debug.Log($"player orientation.transform.rotation: {orientation.transform.eulerAngles}");
+
+        //rotate player object
+        float horizontal_input = input_movement.x;
+        float vertical_input = input_movement.y;
+        Vector3 input_direction = orientation.forward * vertical_input + orientation.right * horizontal_input;
+
+        if (input_direction != Vector3.zero)
+        {
+            obj_player.forward = Vector3.Slerp(obj_player.forward, input_direction.normalized, Time.deltaTime * rotation_speed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    public void inputUpdateMovement(InputAction.CallbackContext context)
+    {
+        if (context.performed) //key pressed
+        {
+            input_movement.x = context.ReadValue<Vector2>().x;
+            input_movement.y = context.ReadValue<Vector2>().y;
+        }
+
+        if (context.canceled) //key released
+        {
+            input_movement.x = context.ReadValue<Vector2>().x;
+            input_movement.y = context.ReadValue<Vector2>().y;
+        }
+    }
+
+    private void movePlayer()
+    {
+        move_direction = orientation.forward * input_movement.y + orientation.right * input_movement.x; //calculate direction
+        if (grounded) { rigidbd.AddForce(move_direction.normalized * move_speed * 10f, ForceMode.Force); } //on ground
+        else { rigidbd.AddForce(move_direction.normalized * move_speed * 10f * air_multiplier, ForceMode.Force); } //on air
+    }
+
+    private void speedControl()
+    {
+        Vector3 flat_velocity = new Vector3(rigidbd.velocity.x, 0f, rigidbd.velocity.z);
+
+        if (flat_velocity.magnitude > move_speed) //limit velocity if needed
+        {
+            Vector3 limited_velocity = flat_velocity.normalized * move_speed;
+            rigidbd.velocity = new Vector3(limited_velocity.x, rigidbd.velocity.y, limited_velocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rigidbd.velocity = new Vector3(rigidbd.velocity.x, 0f, rigidbd.velocity.z); //reset y velocity
+        rigidbd.AddForce(transform.up * jump_force, ForceMode.Impulse);
+    }
+
+    private void resetJump() { ready_to_jump = true; }
+
+    public void inputJump(InputAction.CallbackContext context)
+    {
+        if (context.performed) //key pressed
+        {
+            if (ready_to_jump && grounded)
+            {
+                ready_to_jump = false;
+                Jump();
+                Invoke(nameof(resetJump), jump_cooldown);
+            }
+            is_jumping = true;
+        }
+
+        if (context.canceled) //key released
+        {
+            is_jumping = false;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*public GameObject obj_player;
+    public GameObject obj_player_cam;
+
     private CharacterController controller;
 
     private Vector3 movement;
@@ -41,6 +199,8 @@ public class scr_player_controller : MonoBehaviour
         {
             movement.x = context.ReadValue<Vector2>().x * player_speed;
             movement.z = context.ReadValue<Vector2>().y * player_speed;
+            //Debug.Log($"context = X: {context.ReadValue<Vector2>().x} | Y: {context.ReadValue<Vector2>().y}");
+            //Debug.Log($"movement.x = {movement.x} | movement.z = {movement.z}");
 
             if (movement.x != 0 || movement.z != 0) { is_movement_pressed = true; }
             else { is_movement_pressed = false; }
@@ -132,5 +292,7 @@ public class scr_player_controller : MonoBehaviour
 
         playerGravity();
         if (controller.isGrounded) { jumps = 0; }
-    }
+        
+        //Debug.Log($"obj_player_cam.rotation = {obj_player_cam.transform.rotation.eulerAngles.normalized.y}"); //DEBUG
+    }*/
 }
